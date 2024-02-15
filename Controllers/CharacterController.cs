2a79aebbc26d8ed51a7e5f8e90b78147c8f95019
dgroumpos <dngroumpos@hotmail.com>
2024-02-15 20:23:01 +0200

    fixed program.cs

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
